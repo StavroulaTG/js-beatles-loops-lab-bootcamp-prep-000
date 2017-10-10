8d55d05d9636c9e38800f106d4c780611331d6b6
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-10 18:03:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  const  arr = []
+
+    for (let i=0; i < musicians.length; i++) {
+        arr.push(musicians[i] + " plays " + instruments[i])
+    }
+    return arr
+}
