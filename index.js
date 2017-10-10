98765991b5a4f86f468355980ca8398163465fcf
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-10 18:05:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function theBeatlesPlay(musicians, instruments) {
   const  arr = []
 
-    for (var i=0; i < musicians.length; i++) {
+    for (const i=0; i < musicians.length; i++) {
         arr.push(musicians[i] + " plays " + instruments[i])
     }
     return arr
 }
 
-function johnLennonFacts
+
